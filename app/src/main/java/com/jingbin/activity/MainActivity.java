fcd8accb58f97865db5a22d2c0a03100aa73ea22
Jingbin <805158080@qq.com>
2017-03-03 10:45:04 +0800

    调整包

--- app/src/main/java/com/jingbin/activity/MainActivity.java
+++ app/src/main/java/com/jingbin/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jingbin.animationpractice;
+package com.jingbin.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jingbin.animationpractice.R;
 
 public class MainActivity extends AppCompatActivity {
 
